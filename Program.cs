ecb4304f6f8eb2d869f3d8fbdb86ec79ac3779ad
mmx771 <cosenttino97@gmail.com>
2025-04-25 21:51:24 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Servicios necesarios
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer(); // Esto sí es útil aunque no uses Swagger
 
 var app = builder.Build();
 
-// IMPORTANTE: Habilita el routing ANTES del mapeo de controladores
-app.UseRouting();
-app.UseDefaultFiles();
 app.UseStaticFiles();
-app.UseAuthorization();
+app.UseRouting();
+app.MapControllers();
 
-// Mapea los controladores con su routing
+// Servir index.html en la raÃ­z
 app.MapGet("/", async context =>
 {
     context.Response.ContentType = "text/html";
     await context.Response.SendFileAsync("wwwroot/index.html");
 });
-
-
-// Configurar el puerto dinámico de Render
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Urls.Add($"http://*:{port}");
 
 app.Run();
